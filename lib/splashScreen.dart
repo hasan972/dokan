e7a1ba8934ce737ec79445725718878bd3b250df
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2024-06-01 10:04:40 +0600

    Initial commit: Streamlined codebase, introduced splash screen, and organized folders for enhanced project structure.

---
+++ lib/splashScreen.dart
@@ -0,0 +1,21 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:dokan/UI/login.dart';
+import 'package:flutter/material.dart';
+
+
+class splashScreen extends StatelessWidget {
+  const splashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AnimatedSplashScreen(
+        duration: 2000,
+        splash: Image.asset('assets/images/dokan.PNG'),
+        nextScreen: LoginPage() ,
+        splashTransition: SplashTransition.fadeTransition,
+         backgroundColor: const Color.fromRGBO(248, 248, 248, 1)
+      ),
+    );
+  }
+}
